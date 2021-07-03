fa3e9bcc3250d536deae1b3e345df7ce9b4e64a1
sradda-meduri <79389513+sradda-meduri@users.noreply.github.com>
2021-07-03 22:54:01 +0530

    Sorting Algorithms
    Sorting Algorithms

---
+++ src/main/java/in/sradda/SortingAlgorithms/InsertionSort.java
@@ -0,0 +1,24 @@
+package in.sradda.SortingAlgorithms;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+
+        int[] intArray = {50,6,-5,23,75,-9};
+
+        for(int firstUnsortedIndex = 1 ; firstUnsortedIndex < intArray.length ; firstUnsortedIndex++){
+            int newElement = intArray[firstUnsortedIndex];
+
+            int i;
+
+            for(i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--){
+                intArray[i] = intArray[i - 1];
+            }
+
+            intArray[i] = newElement;
+        }
+
+        for(int i =0;i< intArray.length;i++){
+            System.out.println(intArray[i]);
+        }
+    }
+}
