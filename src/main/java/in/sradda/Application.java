d13512848a423e35a6dcfddb36cd0e89bef62bc0
sradda-meduri <79389513+sradda-meduri@users.noreply.github.com>
2021-07-22 15:37:40 +0530

    Stacks
    Stacks implementation using Arrays and Linked List

--- src/main/java/in/sradda/Application.java
+++ src/main/java/in/sradda/Application.java
@@ -3,6 +3,6 @@ package in.sradda;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello ");
     }
 }
